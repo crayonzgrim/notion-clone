ffe25d5eea9b9fe155abf2179f6f9799f40ab812
crayonzgrim <crayonzgrim@gmail.com>
2023-11-28 17:35:07 +0900

    :hammer: Authentication with convex and clerk

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,8 +4,15 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export default function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +24,29 @@ export default function Navbar() {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log-in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Get Notion free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild={true}>
+              <Link href="/docments">Enter Notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://summary-marten-69.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
