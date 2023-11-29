0c5af3089ae319148c853e2df5e4d697da2a7820
crayonzgrim <crayonzgrim@gmail.com>
2023-11-29 17:10:14 +0900

    :hammer: Searchbar Item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,19 +1,28 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { ChevronsLeft, MenuIcon } from 'lucide-react';
+import {
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings
+} from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 import UserItem from './userItem';
-import { useQuery } from 'convex/react';
+import { useMutation, useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
+import Item from './Item';
+import { toast } from 'sonner';
 
 export default function Navigation() {
   const pathName = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
 
   const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<'aside'>>(null);
@@ -102,6 +111,16 @@ export default function Navigation() {
     }
   }, [pathName, isMobile]);
 
+  const handleCreate = () => {
+    const promise = create({ title: 'Untitled' });
+
+    toast.promise(promise, {
+      loading: 'Create a new note...',
+      success: 'New note created',
+      error: 'Failed to create a new note'
+    });
+  };
+
   return (
     <>
       <aside
@@ -124,6 +143,18 @@ export default function Navigation() {
         </div>
         <div>
           <UserItem />
+          <Item
+            label="Search"
+            icon={Search}
+            isSearch
+            handleOnClick={() => {}}
+          />
+          <Item label="Settings" icon={Settings} handleOnClick={() => {}} />
+          <Item
+            handleOnClick={handleCreate}
+            label="New page"
+            icon={PlusCircle}
+          />
         </div>
         <div className="mt-4">
           {documents?.map((document) => <p>{document.title}</p>)}
